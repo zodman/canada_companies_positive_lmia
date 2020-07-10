0ea561ff724ef1429834fad6c720bd1eb101c67f
Andres Vargas <andrevargas@coca-cola.com>
2020-07-10 11:15:11 -0500

    replace bash scritps with a bakefile

--- fillempty.py
+++ fillempty.py
@@ -6,17 +6,17 @@ filename = sys.argv[1]
 
 results = rows.import_from_csv(filename, encoding='latin1')
 
-
+DELETE_LINES = 8 * -1
 new_results = []
 
 old_obj = {}
-for r in tqdm.tqdm(results):
+for r in tqdm.tqdm(results[:DELETE_LINES]):
     obj = r._asdict()
-    for k,v in obj.items():
+    for k, v in obj.items():
         if v == '':
             obj[k] = old_obj[k]
     new_results.append(obj)
-    old_obj =obj.copy()
+    old_obj = obj.copy()
 
 table = rows.import_from_dicts(new_results)
 rows.export_to_csv(table, f"{filename}.processed.csv")
