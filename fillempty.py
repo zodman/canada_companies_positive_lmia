75d26bd51d597849a251fb4550f44c316e28d6b2
Andres Vargas <andrevargas@coca-cola.com>
2020-07-10 14:16:37 -0500

    adding a new column

--- fillempty.py
+++ fillempty.py
@@ -10,13 +10,15 @@ DELETE_LINES = 8 * -1
 new_results = []
 
 old_obj = {}
-for r in tqdm.tqdm(results[:DELETE_LINES]):
+for r in results[:DELETE_LINES]:
     obj = r._asdict()
+    phase = filename.lower()
+    phase = phase.replace("_positive_en.csv", "")
     for k, v in obj.items():
         if v == '':
             obj[k] = old_obj[k]
+    obj.update({'phase': phase})
     new_results.append(obj)
     old_obj = obj.copy()
-
 table = rows.import_from_dicts(new_results)
 rows.export_to_csv(table, f"{filename}.processed.csv")
