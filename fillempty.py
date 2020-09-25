f4f22ce08b6be009ad40cab280c7cb4b73c85f1d
Andres Vargas <andres@python3.ninja>
2020-09-25 14:05:43 -0500

    update 2020Q2

--- fillempty.py
+++ fillempty.py
@@ -1,5 +1,9 @@
 import sys
 import rows
+import re
+import tqdm
+import time
+
 
 filename = sys.argv[1]
 
@@ -9,20 +13,36 @@ DELETE_LINES = 8 * -1
 new_results = []
 
 old_obj = {}
-for r in results[:DELETE_LINES]:
+
+
+def get_phase(f):
+    r = [
+        "_Positive_Employers_EN.csv",
+        "_Positive_Employer_EN.csv",
+        "_Positive_EN.csv",
+        "_Positive_Employer_Stream_EN.csv",
+        "_Positive_Employer_EN.csv",
+        "_Positive_Employer_EN.csv",
+        "_Positive_Employer_EN.csv",
+        "_Positive_EN.csv",
+        "_employer_positive_EN.csv",
+        "_Positive_EN.csv",
+        "TFWP_",
+    ]
+    for i in r:
+        f = f.replace(i, "")
+    return "{}".format(f)
+
+
+for r in tqdm.tqdm(results[:DELETE_LINES]):
     obj = r._asdict()
-    phase = filename.lower()
-    phase = phase.replace("_positive_en.csv", "")
-    phase = phase.replace("_positive_employer_en.csv", "")
-    phase = phase.replace("_positive_employer_stream_en.csv", "")
-    phase = phase.replace("_employer", "")
-    phase = phase.replace("_positives_en", "")
-    print(phase)
+    phase = get_phase(filename)
     for k, v in obj.items():
         if v == '':
             obj[k] = old_obj[k]
     obj.update({'phase': phase})
     new_results.append(obj)
     old_obj = obj.copy()
+
 table = rows.import_from_dicts(new_results)
 rows.export_to_csv(table, f"{filename}.processed.csv")
--- findjob.py
+++ findjob.py
@@ -3,6 +3,10 @@ import rows
 import feedparser
 import urllib.parse
 import tqdm
+import geopy
+import time
+from geopy.geocoders import GoogleV3, Nominatim, DataBC
+import json
 
 JOBURL ="https://www.jobbank.gc.ca/jobsearch/feed/jobSearchRSSfeed?fage=2&empl={}&sort=D&rows=100"
 
@@ -10,17 +14,32 @@ file = sys.argv[1]
 
 results = rows.import_from_csv(file)
 l = []
-for r in tqdm.tqdm(results[:]):
-    i = r._asdict()
+
+def job(i, employer):
     employer = r.employer
     feed = urllib.parse.quote_plus(employer)
     url = JOBURL.format(feed)
     resp = feedparser.parse(url)
     joblen = len(resp["entries"])
     i.update({'jobs': joblen, 'joburl': 'https://www.jobbank.gc.ca/jobsearch/jobsearch?empl=' + feed })
+
+def loc(i, employer):
+    print(employer)
+    if employer.strip():
+        res = geolocator.geocode(employer)
+        if res:
+            print(res.raw)
+            i.update({'latitude': res.latitude, 'longitude': res.longitude})
+
+
+geolocator = DataBC()
+
+for r in tqdm.tqdm(results[:]):
+    i = r._asdict()
+    loc(i, i.get("address"))
     l.append(i)
 table = rows.import_from_dicts(l)
-rows.export_to_csv(table, file +".joburl.csv")
+rows.export_to_csv(table, file +".loc.csv")
 
 
 
