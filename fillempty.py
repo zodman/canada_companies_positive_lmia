e5058f5204185eeff2d5e5cc518a9ac289e28bd0
Andres Vargas <andres@python3.ninja>
2021-08-07 12:03:14 -0500

    update

--- fillempty.py
+++ fillempty.py
@@ -3,6 +3,8 @@ import rows
 import re
 import tqdm
 import time
+import json
+import hashlib
 
 
 filename = sys.argv[1]
@@ -33,6 +35,12 @@ def get_phase(f):
         f = f.replace(i, "")
     return "{}".format(f)
 
+def str_dec(x): 
+    try:
+        return x.encode("latin1").decode("utf-8")
+    except UnicodeDecodeError:
+        return x
+
 
 for r in tqdm.tqdm(results[:DELETE_LINES]):
     obj = r._asdict()
@@ -43,13 +51,21 @@ for r in tqdm.tqdm(results[:DELETE_LINES]):
     postal_code = "N/A"
     city ="N/A"
     if 'address' in obj:
-        tmp_postal_code = "".join(obj["address"].split(" ")[-2:])
+        address = str_dec(obj["address"])
+        tmp_postal_code = "".join(address.split(" ")[-2:])
         if len(tmp_postal_code) <=6:
             postal_code = tmp_postal_code
-        city = obj["address"].split(",")[0]
+        city = address.split(",")[0]
+        # city = city.decode("utf-8")
+        # if "montr" in city.lower():
+            # assert False, (city.encode("utf-8"),)
 
+
+    idhash=hashlib.md5(json.dumps(obj, sort_keys=True).encode("utf-8")).hexdigest()
 #    tqdm.tqdm.write(postal_code)
-    obj.update({'phase': phase, 'zipcode': postal_code, 'city': city})
+    #if "montr" in city.lower():
+    #    tqdm.tqdm.write("{}".format(city))
+    obj.update({'hashid': idhash, 'phase': phase, 'zipcode': postal_code, 'city': city, 'address':address})
     new_results.append(obj)
     old_obj = obj.copy()
 
